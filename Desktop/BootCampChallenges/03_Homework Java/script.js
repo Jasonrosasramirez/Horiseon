60ea2963b28c739b6cbb33610a5142fd4fdb943a
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-02-25 09:45:34 -0600

    pushing to github

--- Desktop/BootCampChallenges/03_Homework Java/script.js
+++ Desktop/BootCampChallenges/03_Homework Java/script.js
@@ -1,14 +1,49 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); // This is the element in generateBtn
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
+  
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
+  console.log("hey, I worked");
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// element.addEventListener(event, function, useCapture);
+generateBtn.addEventListener("click", writePassword); 
+
+
+
+
+
+
+
+// Who likes comments :) 
+
+/* 
+24Feb2021
+. Using the chrome inspector, sources states funtion generatePassword() is undefined. 
+. 
+.
+.
+.
+.
+
+*/
+
+
+
+
+
+
+
